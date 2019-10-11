cb669124305b9e7a6058505ff716872a26da7454
JoeBig7 <1479762535@qq.com>
2019-10-11 10:43:36 +0800

    :recycle:

---
+++ springboot-example-data-jpa/src/main/java/com/mamba/jpa/SpringbootExampleDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mamba.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExampleDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExampleDataJpaApplication.class, args);
+	}
+
+}
